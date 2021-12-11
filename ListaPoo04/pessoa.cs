bf98a8571e3d331b018a0a4112ce4d35d0695234
Deivdson <94084592+Deivdson@users.noreply.github.com>
2021-12-11 20:27:35 -0300

    Create pessoa.cs

---
+++ ListaPoo04/pessoa.cs
@@ -0,0 +1,28 @@
+class MainClass {
+  public static void Main() {
+    Pessoa a = new Pessoa("a", 20);
+    Pessoa b = new Pessoa("b", 45);
+    Pessoa c = new Pessoa("c", 45);
+    a.SetFiliacao(b, c);
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+    Console.WriteLine(c);
+  }
+}
+class Pessoa {
+  private string nome;
+  private int idade;
+  private Pessoa pai;
+  private Pessoa mae;
+    public Pessoa(string nome, int idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+  public void SetFiliacao(Pessoa pai, Pessoa mae) {
+    this.pai = pai;
+    this.mae = mae;
+  }
+  public override string ToString() {
+    return $"{nome} {idade} {pai} {mae}";
+  }
+}
